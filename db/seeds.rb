734e4d76a124b1896b6e3d56970dc43037ddef65
mickael joseph <joseph.mickk@gmail.com>
2020-08-12 17:09:05 +0200

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+dishoom    = { name: "So Good", address: "euratech", phone_number: "89 98 65 78 89", category: "french" }
+pizza_east = { name: "La famille", address: "valenciennes", phone_number: "09 87 97 09 98", category: "italian" }
+pizza_enzo    = { name: "enzo", address: "saint am", phone_number: "06 78 89 09 90", category:"italian"}
+kebab = { name: "kebab", address: "tournai", phone_number: "05 89 76 90 76", category: "belgian" }
+friterie    = { name: "fiesta", address: "lecelles", phone_number: "04 76 90 76 23", category: "french" }
+
+[ dishoom, pizza_east, pizza_enzo, kebab, friterie ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+end
